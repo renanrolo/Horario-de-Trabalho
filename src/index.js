f5d1c62a566d8679de51e97dde08879e97d05975
Renan Rolo <renanrolo@hotmail.com>
2022-03-02 08:18:52 -0400

    adding google analitycs and changing build folder to default

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app/App';
 import { Provider } from 'react-redux';
 import  'bootstrap/dist/css/bootstrap.css';
+import ReactGA from 'react-ga';
 
+import App from './app/App';
 import Store from './app/store'
+
+const TRACKING_ID = "G-WVHZZFPGZX"; // YOUR_OWN_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 ReactDOM.render(
     <Provider store={Store}>
