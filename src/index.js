aff5d6f0557094cdf9cf39c7553e40c12a3d168d
Renan Rolo <renanrolo@hotmail.com>
2022-03-05 11:47:16 -0400

    build

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ReactGA from 'react-ga';
 import App from './app/App';
 import Store from './app/store'
 
-const TRACKING_ID = "G-WVHZZFPGZX";
+const TRACKING_ID = "G-0HJ9DJQVNE";
 ReactGA.initialize(TRACKING_ID, {
     gaOptions: {
         cookieFlags: "SameSite=None; Secure",
